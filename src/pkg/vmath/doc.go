29a0d918d250cbb9b44fe4f7203a69b64edd67c3
Philip Rideout <prideout@gmail.com>
2012-02-11 01:19:04 -0800

    rearranging

---
+++ src/pkg/vmath/doc.go
@@ -0,0 +1,9 @@
+// Simple linear algebra solely for 3D graphics.
+// Defines structs and functions for points, vectors, and small matrices.
+package vmath
+
+// Largely inspired by:
+// https://bitbucket.org/prideout/pez-viewer/src/11899f6b6f02/vmath.h
+
+// Steal most of the stuff in README.textile
+// Look at $GOROOT/src/pkg/html/template/doc.go for a formatting example
